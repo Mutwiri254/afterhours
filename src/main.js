e2aa6e874611d1f42653c94f4e86f75935b873e2
unknown <morrismutwiri254@gmail.com>
2026-02-07 17:12:07 +0300

    Week 1 added Ambient sound + soft arrival fade

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
 console.log("AfterHours is open.");
+
+const ambience = new Audio("../assets/ambience.mp3");
+ambience.loop = true;
+ambience.volume = 0.3;
+
+window.addEventListener("load", () => {
+  document.body.classList.add("visible");
+});
+
+// Browsers require user interaction before playing audio
+document.addEventListener("click", () => {
+  if (ambience.paused) {
+    ambience.play();
+    console.log("Ambience started.");
+  }
+});
